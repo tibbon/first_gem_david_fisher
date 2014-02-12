1e26285dca8d557e1c038f3774b95cf3e7e9b5b2
David Fisher <tibbon@gmail.com>
2014-02-12 11:38:28 -0500

    Added a unique words method. Its awesome

--- lib/first_gem_david_fisher.rb
+++ lib/first_gem_david_fisher.rb
@@ -1,5 +1,11 @@
 require "first_gem_david_fisher/version"
 
-module FirstGemDavidFisher
-  # Your code goes here...
+class String
+  def word_count
+    self.split.count
+  end
+
+  def unique_words
+    self.split.uniq
+  end
 end
